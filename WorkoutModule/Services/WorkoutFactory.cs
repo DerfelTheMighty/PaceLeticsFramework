8f46ce7a30cdc21c119cd3fbfa272d103abe85f8
Christoph Otte <christoph@caotte.de>
2023-11-15 01:09:44 +0100

    bugfix

--- WorkoutModule/Services/WorkoutFactory.cs
+++ WorkoutModule/Services/WorkoutFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkoutModule.Contracts;
+﻿using WorkoutModule.Contracts;
 using WorkoutModule.Logic;
 using WorkoutModule.Models;
 
