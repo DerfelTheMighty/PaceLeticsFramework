2e8fcd52f2eecb28fd8fa416be14e65aa74b6e00
Christoph Otte <christoph@caotte.de>
2025-11-27 01:26:50 +0100

    fix typos remove deprecated pacecard

--- PaceLetics.VdotModule.Components/PaceInfo.razor.cs
+++ PaceLetics.VdotModule.Components/PaceInfo.razor.cs
@@ -34,23 +34,23 @@ namespace PaceLetics.VdotModule.Components
         {
             Items = new()
         {
-            new("E Pace – Grundlagenausdauer", "/images/icons/epace.png",
+            new("E Pace - Grundlagenausdauer", "/images/icons/epace.png",
                 Upper: EPaceHigh, Lower: EPaceLow,
                 Description: EText),
 
-            new("M Pace – Marathon", "/images/icons/mpace.png",
+            new("M Pace - Marathon", "/images/icons/mpace.png",
                 Upper: MPaceHigh, Lower: MPaceLow,
                 Description: MText),
 
-            new("T Pace – Schwelle", "/images/icons/tpace.png",
+            new("T Pace - Schwelle", "/images/icons/tpace.png",
                 Upper: TPaceHigh, Lower: TPaceLow,
                 Description: TText),
 
-            new("I Pace – VO2max", "/images/icons/ipace.png",
+            new("I Pace - VO2max", "/images/icons/ipace.png",
                 Upper: IPaceHigh, Lower: IPaceLow,
                 Description: IText),
 
-            new("R Pace – Repetition", "/images/icons/rpace.png",
+            new("R Pace - Repetition", "/images/icons/rpace.png",
                 Upper: RPaceHigh, Lower: RPaceLow,
                 Description: RText)
         };
@@ -59,25 +59,25 @@ namespace PaceLetics.VdotModule.Components
 
  
         private string EText = @"Die E-Pace bildet das Fundament deiner aeroben Ausdauer. 
-Sie liegt deutlich unter der Schwelle und wird ueberwiegend ueber den Fettstoffwechsel getragen. 
-Ein grosser Teil deiner Wochenkilometer sollte in diesem Tempo stattfinden. 
+Sie liegt deutlich unter der Schwelle und wird zu einem großen Teil über den Fettstoffwechsel getragen. 
+Die meisten deiner Wochenkilometer sollten in diesem Tempo stattfinden. 
 Die angegebene Geschwindigkeit beschreibt den oberen GA1-Bereich.";
 
         private string MText = @"Die M-Pace entspricht deinem realistischen Marathonrenntempo. 
-Sie ist ueberwiegend aerob, aber kohlenhydratintensiver als E-Pace. 
-Viele empfinden hier einen stabilen Flow. 
-Fuer reines Grundlagentraining ist sie zu schnell.";
+Sie ist überwiegend aerob, aber kohlenhydratintensiver als E-Pace. 
+Viele empfinden hier einen angenehmen Flow. 
+Für reines Grundlagentraining ist sie aber viel zu schnell.";
 
         private string TText = @"Die T-Pace liegt nahe deinem 10-km-Tempo und verbindet hohe aerobe und anaerobe Anteile. 
-Dieses Tempo kannst du ungefaehr eine Stunde halten. 
-Fortgeschrittene nutzen es fuer laengere Intervalle, Einsteigende fuer kuerzere Abschnitte.";
+Dieses Tempo kannst du ungefähr eine Stunde halten. 
+Fortgeschrittene nutzen es für längere Intervalle, Einsteigende auch für kürzere Abschnitte.";
 
         private string IText = @"Die I-Pace liegt deutlich oberhalb der Schwelle und setzt starke Reize auf deine VO2max. 
-Intervalle sind kurz, die Pausen zu kurz fuer eine komplette Erholung.";
+Intervalle sind kurz, die Pausen zu kurz für eine komplette Erholung.";
 
         private string RText = @"Die R-Pace ist sehr schnell und technisch anspruchsvoll. 
-Sie verbessert Laufoekonomie, Kraft und Koordination. 
+Sie verbessert Laufökonomie, Kraft und Koordination. 
 Intervalle sind kurz, Pausen lang. 
-Vor allem fuer erfahrene Laeufer geeignet.";
+Vor allem für erfahrene Läufer:innen geeignet.";
     }
 }
