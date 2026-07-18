8086f2bdc90d1a33ec7d20b214d72b591f0a0474
Christoph Otte <christoph@caotte.de>
2026-07-18 12:15:07 +0200

    Extend Critical Speed chart to marathon distance

--- PaceLetics.CoreModule.Infrastructure/Constants/RaceDistance.cs
+++ PaceLetics.CoreModule.Infrastructure/Constants/RaceDistance.cs
@@ -6,6 +6,7 @@ namespace PaceLetics.CoreModule.Infrastructure.Constants
 		public const long D1200Meters = 1200;
 		public const long D3000Meters = 3000;
 		public const long D3600Meters = 3600;
+		public const long DMarathonMeters = 42195;
 
 		public static Dictionary<string, long> Dict { get; private set; } = new Dictionary<string, long>()
 		{ {"1 km",1000},
@@ -16,7 +17,7 @@ namespace PaceLetics.CoreModule.Infrastructure.Constants
 		  {"10 km", 10000 },
 		  {"15 km", 15000 },
 		  {"Halbmarathon", 21097 },
-		  {"Marathon", 42195 }
+		  {"Marathon", DMarathonMeters }
 		};
 	}
 }
